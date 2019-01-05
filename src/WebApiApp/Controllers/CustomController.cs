5bd4fa6e98aef1cc1f48310d462ad5aa8529b939
noxi515 <tzo.ppl@gmail.com>
2019-01-05 18:33:40 +0900

    独自規約の実装

---
+++ src/WebApiApp/Controllers/CustomController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    [ApiConventionType(typeof(CustomApiConventions))]
+    public class CustomController : ControllerBase
+    {
+        /// <summary>
+        /// 新規でモデルを登録するアクションメソッド<br />
+        /// `POST api/custom`
+        /// </summary>
+        [HttpPost]
+        public ActionResult<CustomModel> Create([FromBody] CustomModel model)
+        {
+            return Created($"api/custom/{model.Id}", model);
+        }
+    }
+
+    /// <summary>
+    /// 対象のモデル
+    /// </summary>
+    public class CustomModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/WebApiApp/CustomApiConventions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace WebApiApp
+{
+    /// <summary>
+    /// 独自のWebAPI規約クラス
+    /// </summary>
+    public static class CustomApiConventions
+    {
+        /// <summary>
+        /// 作成するメソッドのWebAPI規約
+        /// </summary>
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesDefaultResponseType]
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        public static void Create(
+            [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]
+            [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+            object model)
+        {
+        }
+    }
+}
